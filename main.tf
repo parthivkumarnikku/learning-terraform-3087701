c9185e734ab55b9e59d41b611e3e11498b35d83b
Parthiv Kumar Nikku <116823475+parthivkumarnikku@users.noreply.github.com>
2026-09-08 14:29:33 +0530

    Add IAM instance profile to EC2 configuration

--- main.tf
+++ main.tf
@@ -77,3 +77,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
+
+iam_instance_profile = aws_iam_instance_profile.lab_ec2_profile.name
