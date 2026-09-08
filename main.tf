d9ea8b13c04bd2790e16fdf1314ef3cf1383b796
Parthiv Kumar Nikku <116823475+parthivkumarnikku@users.noreply.github.com>
2026-09-08 16:32:19 +0530

    Add SSH ingress rule to blog security group

--- main.tf
+++ main.tf
@@ -79,4 +79,12 @@ resource "aws_security_group_rule" "blog_everything_out" {
   security_group_id = aws_security_group.blog.id
 }
 
+resource "aws_security_group_rule" "lab_runner_ssh_in" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.blog_sg.security_group_id
+}
 
