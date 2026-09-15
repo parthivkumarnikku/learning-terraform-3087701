837b5e5117c487a14b906d31f5f863c5c492002c
Parthiv Kumar Nikku <116823475+parthivkumarnikku@users.noreply.github.com>
2026-09-15 22:43:56 +0530

    fix terraform formatting isssues

--- main.tf
+++ main.tf
@@ -34,12 +34,12 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id              = data.aws_vpc.default.id
+  name                = "blog"
+  ingress_rules       = ["https-443-tcp", "http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
@@ -62,21 +62,21 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type               = "ingress"
-  from_port          = 443
-  to_port            = 443
-  protocol           = "tcp"
-  cidr_blocks        = ["0.0.0.0/0"]
-  security_group_id  = aws_security_group.blog.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
@@ -88,4 +88,3 @@ resource "aws_security_group_rule" "lab_runner_ssh_in" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.blog_sg.security_group_id
 }
-
