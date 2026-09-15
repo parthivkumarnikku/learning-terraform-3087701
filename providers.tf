6ed8b4383cac5fe41f681f750e3dc357389e2cea
Parthiv Kumar Nikku <116823475+parthivkumarnikku@users.noreply.github.com>
2026-09-15 22:53:29 +0530

    change version to match terraform run

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Add a version constraint
+      version = "~> 6.0" # Add a version constraint
     }
   }
 }
