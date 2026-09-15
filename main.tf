79e0b08ba09ec4c84e87464ecf03e64bae687f60
Parthiv Kumar Nikku <116823475+parthivkumarnikku@users.noreply.github.com>
2026-09-15 22:36:44 +0530

    fix terraform formatting issues

--- main.tf
+++ main.tf
@@ -62,12 +62,12 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  type               = "ingress"
+  from_port          = 443
+  to_port            = 443
+  protocol           = "tcp"
+  cidr_blocks        = ["0.0.0.0/0"]
+  security_group_id  = aws_security_group.blog.id
 }
 
 
