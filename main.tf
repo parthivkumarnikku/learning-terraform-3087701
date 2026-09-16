e86930c70e07faadd7c137ce39055d5b710fd6b5
Parthiv Kumar Nikku <116823475+parthivkumarnikku@users.noreply.github.com>
2026-09-16 10:44:02 +0530

    This commit is to trigger run
    Added comment to indicate policy violation for SSH access. to trigger run

--- main.tf
+++ main.tf
@@ -85,6 +85,6 @@ resource "aws_security_group_rule" "lab_runner_ssh_in" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"] # This part of code is violating the policy for ssh
   security_group_id = module.blog_sg.security_group_id
 }
